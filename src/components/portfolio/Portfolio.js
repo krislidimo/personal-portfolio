312467a8a7fa86a94ef698c3132d334c0543c1ea
Krisli Dimo <krislidimo@gmail.com>
2019-09-03 21:58:29 -0500

    imported PortfolioCarousel

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import PortfolioNav from './portfolioNav/PortfolioNav.js';
+import PortfolioCarousel from './portfolioCarousel/PortfolioCarousel.js';
 
 function Portfolio() {
   return (
     <div className="portfolio">
       <PortfolioNav />
+      <PortfolioCarousel />
     </div>
   );
 }
