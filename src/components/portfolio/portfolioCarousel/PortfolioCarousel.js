05cd7f9613b1980b9fff3d4a9fb8431868f31b85
Krisli Dimo <krislidimo@gmail.com>
2019-09-03 22:00:32 -0500

    added PortfolioCarousel.js

---
+++ src/components/portfolio/portfolioCarousel/PortfolioCarousel.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+class PortfolioCarousel extends React.Component {
+  render () {
+  	const settings = {
+		  className: "center",
+		  centerMode: true,
+		  infinite: true,
+		  centerPadding: "60px",
+		  slidesToShow: 3,
+		  speed: 500
+		};
+
+	  return (
+	    <div className="portfolio-carousel">
+	    	<Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+	    </div>
+	  );
+	}
+}
+
+export default PortfolioCarousel;
