58b1e759be41482a958f7f17927525fe714f34b0
Krisli Dimo <krislidimo@gmail.com>
2019-09-03 20:34:51 -0500

    added PortfolioNav.js

---
+++ src/components/portfolio/portfolioNav/PortfolioNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function PortfolioNav() {
+  return (
+    <div className="portfolio-nav">
+    	<h2 className="name">Krisli Dimo</h2>
+    	<nav>
+	    	<a className="nav-item">About</a>
+  	  	<a className="nav-item">Portfolio</a>
+  	  </nav>
+    </div>
+  );
+}
+
+export default PortfolioNav;
