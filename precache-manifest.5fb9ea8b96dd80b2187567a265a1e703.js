4eafb64235eea997168194f3cefe297e453a45b8
Krisli Dimo <krislidimo@gmail.com>
2019-09-04 21:17:17 -0500

    Updates

---
+++ precache-manifest.5fb9ea8b96dd80b2187567a265a1e703.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9402c88b60e48d8c7c3af4b09ea60e9",
+    "url": "/krislidimo/personal-portfolio/index.html"
+  },
+  {
+    "revision": "df691f3a63cabacb252c",
+    "url": "/krislidimo/personal-portfolio/static/css/2.66f26aa3.chunk.css"
+  },
+  {
+    "revision": "7a9b4209578c02cb89ad",
+    "url": "/krislidimo/personal-portfolio/static/css/main.578a18ca.chunk.css"
+  },
+  {
+    "revision": "df691f3a63cabacb252c",
+    "url": "/krislidimo/personal-portfolio/static/js/2.8473229c.chunk.js"
+  },
+  {
+    "revision": "7a9b4209578c02cb89ad",
+    "url": "/krislidimo/personal-portfolio/static/js/main.9cd4ebc7.chunk.js"
+  },
+  {
+    "revision": "f50812b07c232147146f",
+    "url": "/krislidimo/personal-portfolio/static/js/runtime~main.bd896ede.js"
+  },
+  {
+    "revision": "1515a780fb282b08e191d59572d94909",
+    "url": "/krislidimo/personal-portfolio/static/media/forest-1.1515a780.png"
+  },
+  {
+    "revision": "0d8f7febe8063b6f64323d2225fa8144",
+    "url": "/krislidimo/personal-portfolio/static/media/forest-2.0d8f7feb.png"
+  },
+  {
+    "revision": "e9315340961af2ee1702c23c5bd513b9",
+    "url": "/krislidimo/personal-portfolio/static/media/forest-3.e9315340.png"
+  },
+  {
+    "revision": "a9d66d1f1404ceaf40e0c21ffee2550b",
+    "url": "/krislidimo/personal-portfolio/static/media/landscape-left.a9d66d1f.png"
+  },
+  {
+    "revision": "718ada9c5af009acd24ea0dcea4d7572",
+    "url": "/krislidimo/personal-portfolio/static/media/landscape-right.718ada9c.png"
+  },
+  {
+    "revision": "f3cba6c1d0e38c256fd0d840032c4083",
+    "url": "/krislidimo/personal-portfolio/static/media/mountain-base.f3cba6c1.png"
+  },
+  {
+    "revision": "d37801ee6b866ba637e4ba7b9f87b22c",
+    "url": "/krislidimo/personal-portfolio/static/media/mountain-mid.d37801ee.png"
+  },
+  {
+    "revision": "a00822e04b522e58bc33c4c6bd44004a",
+    "url": "/krislidimo/personal-portfolio/static/media/mountain-peak.a00822e0.png"
+  },
+  {
+    "revision": "b41458273563d07a34a74f124a1f40a3",
+    "url": "/krislidimo/personal-portfolio/static/media/mountain.b4145827.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/krislidimo/personal-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/krislidimo/personal-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/krislidimo/personal-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/krislidimo/personal-portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
