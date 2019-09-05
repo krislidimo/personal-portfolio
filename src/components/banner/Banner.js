4ae0298665ec371b23fb2819a4acdf6b306af8a2
Krisli Dimo <krislidimo@gmail.com>
2019-09-05 00:50:46 -0500

    commented and cleaned code

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {throttle} from 'lodash';
+import { throttle } from 'lodash';
 
+// Banner Images used as layers
 import mountain from '../../assets/images/mountain/mountain.jpg';
-import landscapeRight from '../../assets/images/mountain/landscape-right.png';
-import landscapeLeft from '../../assets/images/mountain/landscape-left.png';
-import forest1 from '../../assets/images/mountain/forest-1.png';
-import forest2 from '../../assets/images/mountain/forest-2.png';
-import forest3 from '../../assets/images/mountain/forest-3.png';
-import mountainBase from '../../assets/images/mountain/mountain-base.png';
-import mountainMid from '../../assets/images/mountain/mountain-mid.png';
 import mountainPeak from '../../assets/images/mountain/mountain-peak.png';
+import mountainMid from '../../assets/images/mountain/mountain-mid.png';
+import mountainBase from '../../assets/images/mountain/mountain-base.png';
+import forest3 from '../../assets/images/mountain/forest-3.png';
+import forest2 from '../../assets/images/mountain/forest-2.png';
+import forest1 from '../../assets/images/mountain/forest-1.png';
+import landscapeLeft from '../../assets/images/mountain/landscape-left.png';
+import landscapeRight from '../../assets/images/mountain/landscape-right.png';
 
 class Banner extends React.Component {
 
@@ -17,27 +18,19 @@ class Banner extends React.Component {
 		let parent = document.getElementById('parallax-container');
 		let children = parent.getElementsByTagName('div');
 
-		children[0].style.transform = 'translateY(-' + (window.pageYOffset * 0 / children.length) + 'px)';
-		children[1].style.transform = 'translateY(-' + (window.pageYOffset * .2 / children.length) + 'px)';
-		children[2].style.transform = 'translateY(-' + (window.pageYOffset * 0.4 / children.length) + 'px)';
-		children[3].style.transform = 'translateY(-' + (window.pageYOffset * 0.6 / children.length) + 'px)';
-		children[4].style.transform = 'translateY(-' + (window.pageYOffset * 2 / children.length) + 'px)';
-		children[5].style.transform = 'translateY(-' + (window.pageYOffset * 3 / children.length) + 'px)';
-		children[6].style.transform = 'translateY(-' + (window.pageYOffset * 5 / children.length) + 'px)';
-		children[7].style.transform = 'translateY(-' + (window.pageYOffset * 8 / children.length) + 'px)';
-		children[8].style.transform = 'translateY(-' + (window.pageYOffset * 8.6 / children.length) + 'px)';
-		children[9].style.transform = 'translateY(-' + (window.pageYOffset * 12 / children.length) + 'px)';
-		children[10].style.transform = 'translateY(-' + (window.pageYOffset * 12 / children.length) + 'px)';
-	}
-
-	throttle = (fn, wait) => {
-	  var time = Date.now();
-	  return function() {
-	    if ((time + wait - Date.now()) < 0) {
-	      fn();
-	      time = Date.now();
-	    }
-	  }
+		// Set off-set of each layer as you scroll
+		children[0].style.transform = 'translateY(-' + (window.pageYOffset * 0 / children.length) + 'px)';    // mountain
+ 		children[1].style.transform = 'translateY(-' + (window.pageYOffset * 0.2 / children.length) + 'px)';  // mountainPeak
+ 		children[2].style.transform = 'translateY(-' + (window.pageYOffset * 0.4 / children.length) + 'px)';  // mountainMid
+ 		children[3].style.transform = 'translateY(-' + (window.pageYOffset * 0.6 / children.length) + 'px)';  // mountainBase
+ 		children[4].style.transform = 'translateY(-' + (window.pageYOffset * 1 / children.length) + 'px)';    // forest3
+ 		children[5].style.transform = 'translateY(-' + (window.pageYOffset * 3 / children.length) + 'px)';    // forest2
+ 		children[6].style.transform = 'translateY(-' + (window.pageYOffset * 5 / children.length) + 'px)';    // forest1
+ 		children[7].style.transform = 'translateY(-' + (window.pageYOffset * 8 / children.length) + 'px)';    // landscapeLeft
+ 		children[8].style.transform = 'translateY(-' + (window.pageYOffset * 8.6 / children.length) + 'px)';  // landscapeRight
+ 
+		children[9].style.transform = 'translateY(-' + (window.pageYOffset * 12 / children.length) + 'px)';   // top-wrapper
+		children[10].style.transform = 'translateY(-' + (window.pageYOffset * 12 / children.length) + 'px)';  // title
 	}
 
 	componentDidMount() {
@@ -47,83 +40,8 @@ class Banner extends React.Component {
 	render() {
 	  return (
 	    <div className="banner">
-		    {/*<ParallaxBanner
-		    			    className="your-class"
-		    			    layers={[
-		    			    	{
-		    			    		children: <img src={mountain} onLoad={this.handleLoad} alt='Mountain banner image.' />,
-		    	            amount: 1,
-		    	            expanded: false,
-		    			    	},
-		    		        {
-		    	            image: mountain,
-		    	            amount: 0.53,
-		    	            expanded: false,
-		    		        },
-		    		        {
-		    	            image: mountainPeak,
-		    	            amount: 0.48,
-		    	            expanded: false,
-		    		        },
-		    		        {
-		    	            image: mountainMid,
-		    	            amount: 0.46,
-		    	            expanded: false,
-		    		        },
-		    		        // {
-		    	         //    children: <div className='title-wrapper'><h1 className='title'>Full Stack Developer</h1></div>,
-		    	         //    amount: 0.47,
-		    	         //    expanded: false,
-		    		        // },
-		    		        {
-		    	            image: mountainBase,
-		    	            amount: 0.44,
-		    	            expanded: false,
-		    		        },
-		    		        {
-		    	            image: forest3,
-		    	            amount: 0.36,
-		    	            expanded: false,
-		    		        },
-		    		        {
-		    	            image: forest2,
-		    	            amount: 0.30,
-		    	            expanded: false,
-		    		        },
-		    		        {
-		    	            image: forest1,
-		    	            amount: 0.20,
-		    	            expanded: false,
-		    		        },
-		    		        {
-		    	            image: landscapeLeft,
-		    	            amount: 0.05,
-		    	            expanded: false,
-		    		        },
-		    		        {
-		    	            image: landscapeRight,
-		    	            amount: 0.00,
-		    	            expanded: false,
-		    		        },
-		    		        {
-		    	            children: <div className='top-wrapper'>
-		    	            	<h1 className='name'>Krisli Dimo</h1>
-		    	            	<div className='nav'>
-		    	            		<button className='nav-link portfolio-link'>
-		    	            			Portfolio
-		    	            		</button>
-		    	            	</div>
-		    	            </div>,
-		    	            amount: 0,
-		    	            expanded: false,
-		    		        },
-		    			    ]}
-		    			    style={{
-		    		        height: '100vh',
-		    			    }}
-		    				>
-		    				</ParallaxBanner>*/}
 		    <div id="parallax-container">
+		    	{/*Parallax Banner Layers*/}
 					<div className="layer z-index-10" ><img src={mountain} alt='Mountain banner image.' /></div>
 					<div className="layer z-index-9" ><img src={mountainPeak} alt='' /></div>
 					<div className="layer z-index-8" ><img src={mountainMid} alt='' /></div>
@@ -134,6 +52,7 @@ class Banner extends React.Component {
 					<div className="layer z-index-3" ><img src={landscapeLeft} alt='' /></div>
 					<div className="layer z-index-2" ><img src={landscapeRight} alt='' /></div>
 
+					{/*Name & Nav*/}
 					<div className='top-wrapper'>
 			    	<h1 className='name'>Krisli Dimo</h1>
 			    	<div className='nav'>
@@ -143,8 +62,8 @@ class Banner extends React.Component {
 			    	</div>
 			    </div>
 
+			  	{/*Title*/}
 					<div className='title-wrapper layer z-index-8'><h1 className='title'>Full Stack Developer</h1></div>
-
 				</div>
 	    </div>
 	  );
