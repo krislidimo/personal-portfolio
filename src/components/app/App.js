1dbfcd525b5a1a4a83091fa7f3ff197d2796f377
Krisli Dimo <krislidimo@gmail.com>
2019-09-05 00:32:38 -0500

    removed react-scroll-parallax

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import { ParallaxProvider } from 'react-scroll-parallax';
 import SmoothScroll from 'smoothscroll-for-websites';
 
 import Banner from '../banner/Banner.js'
 import Portfolio from '../portfolio/Portfolio.js'
 
 SmoothScroll({ 
-	stepSize: 50,  // [px]
+	stepSize: 40,  // [px]
 	animationTime: 500, // [ms]
 })
 
 function App() {
   return (
     <div className="App">
-    	<ParallaxProvider>
-    		<Banner />
-    	</ ParallaxProvider>
-
+      <Banner />
     	<Portfolio />
     </div>
   );
