5fea99c9d1f7c523da7a22cea709f655e8751875
Krisli Dimo <krislidimo@gmail.com>
2019-09-08 23:40:02 -0500

    converted into class and added project-number

--- src/components/portfolio/projectCard/ProjectCard.js
+++ src/components/portfolio/projectCard/ProjectCard.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import Tilty  from 'react-tilty';
 
-function ProjectCard() {
-  return (
-  	<Tilty
-  		className="tilty"
-  		settings={{
-  			glare: true,
-  			scale: 1.08,
-  			"max-glare": 0.5
-  		}}
-  	>
-    	<div className="project-card">
-    			<h2 className="inner">Hello, World</h2>
-   	 	</div>
-   	</Tilty>
-  );
+
+class ProjectCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+    	<Tilty
+    		className="tilty"
+    		settings={{
+    			glare: true,
+    			scale: 1.08,
+    			"max-glare": 0.5
+    		}}
+    	>
+      	<div className="project-card" style={{backgroundImage: `url("${this.props.project.src}")`}}>
+    			<h2 className="title">{this.props.project.title}</h2>
+          <h3 className="project-number">{this.props.project.id}</h3>
+     	 	</div>
+     	</Tilty>
+    );
+  }
 }
 
 export default ProjectCard;
