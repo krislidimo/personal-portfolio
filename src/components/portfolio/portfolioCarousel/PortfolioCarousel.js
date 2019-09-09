b56d32f6aa5d1161d870a589804f5233fddf9c1a
Krisli Dimo <krislidimo@gmail.com>
2019-09-08 23:34:56 -0500

    added dummy data

--- src/components/portfolio/portfolioCarousel/PortfolioCarousel.js
+++ src/components/portfolio/portfolioCarousel/PortfolioCarousel.js
@@ -6,39 +6,56 @@ import ProjectCard from '../projectCard/ProjectCard.js';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import img1 from "../../../assets/images/It's_Over_9000.PNG";
 
 class PortfolioCarousel extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      projects: [
+        {
+          'title': "It's Over 900",
+          'src': img1,
+          'id': '00'
+        },
+        {
+          'title': "Nifty Market",
+          'src': "",
+          'id': '01'
+        },
+        {
+          'title': "Mympy",
+          'src': "",
+          'id': '02'
+        },
+        {
+          'title': "Macro Calculator",
+          'src': "",
+          'id': '03'
+        }
+      ]
+    }
+  }
+  
   render () {
   	const settings = {
-		  className: "center",
-		  centerMode: true,
 		  infinite: true,
 		  centerPadding: "60px",
 		  slidesToShow: 3,
-		  speed: 500
+      dots: true,
+		  speed: 500,
+      // vertical: true,
+      // verticalSwiping: true,
 		};
 
 	  return (
 	    <div className="portfolio-carousel">
 	    	<Slider {...settings}>
-          <div>
-            <ProjectCard />
-          </div>
-          <div>
-            <ProjectCard />
-          </div>
-          <div>
-            <ProjectCard />
-          </div>
-          <div>
-            <ProjectCard />
-          </div>
-          <div>
-            <ProjectCard />
-          </div>
-          <div>
-            <ProjectCard />
-          </div>
+          {this.state.projects.map(project => {
+            return <div key={project.id}>
+              <ProjectCard project={project}/>
+            </div>
+          })}
         </Slider>
 	    </div>
 	  );
