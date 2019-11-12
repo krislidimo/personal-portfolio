8ed679bb7e0f868765971a54920a0a09331e971a
krislidimo <krislidimo@gmail.com>
2019-11-12 00:29:56 -0600

    added routes to portfolio section

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import PortfolioNav from './portfolioNav/PortfolioNav.js';
 import PortfolioCarousel from './portfolioCarousel/PortfolioCarousel.js';
+import About from './about/About.js';
+
+import { Route } from 'react-router-dom';
 
 function Portfolio() {
   return (
-    <div className="portfolio">
-      <PortfolioNav />
-      <PortfolioCarousel />
+    <div className="portfolio" id="portfolio">
+		<PortfolioNav />
+    	<Route exact path="/" component={PortfolioCarousel} />
+    	<Route path="/portfolio" component={PortfolioCarousel} />
+    	<Route path="/about" component={About} />
     </div>
   );
 }
