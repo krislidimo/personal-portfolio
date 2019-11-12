855fe5a20c4498effe2ba2792513b2f16215c24e
krislidimo <krislidimo@gmail.com>
2019-11-12 00:18:46 -0600

    removed smoothscroll-for-websites setup

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import SmoothScroll from 'smoothscroll-for-websites';
 
 import Banner from '../banner/Banner.js'
 import Portfolio from '../portfolio/Portfolio.js'
 
-SmoothScroll({ 
-	stepSize: 30,  // [px]
-	animationTime: 500, // [ms]
-})
-
 function App() {
   return (
     <div className="App">
-      <Banner />
+		<Banner />
     	<Portfolio />
     </div>
   );
