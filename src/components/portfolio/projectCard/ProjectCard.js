2a3bc0e2c94fdd4cad4a2779b10a219ed15ec375
krislidimo <krislidimo@gmail.com>
2019-11-12 00:37:19 -0600

    removed title from card, adjusted tilty scale

--- src/components/portfolio/projectCard/ProjectCard.js
+++ src/components/portfolio/projectCard/ProjectCard.js
@@ -13,12 +13,11 @@ class ProjectCard extends React.Component {
     		className="tilty"
     		settings={{
     			glare: true,
-    			scale: 1.08,
+    			scale: 1.04,
     			"max-glare": 0.5
     		}}
     	>
       	<div className="project-card" style={{backgroundImage: `url("${this.props.project.src}")`}}>
-    			<h2 className="title">{this.props.project.title}</h2>
           <h3 className="project-number">{this.props.project.id}</h3>
      	 	</div>
      	</Tilty>
