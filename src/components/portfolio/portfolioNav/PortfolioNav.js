433560d6881715b1f9509247de994b53a566b610
krislidimo <krislidimo@gmail.com>
2019-11-12 00:32:00 -0600

    swapped a tags with navlinks

--- src/components/portfolio/portfolioNav/PortfolioNav.js
+++ src/components/portfolio/portfolioNav/PortfolioNav.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import { NavLink } from 'react-router-dom'
+
 function PortfolioNav() {
-  return (
-    <div className="portfolio-nav">
-    	<h2 className="name">Krisli Dimo</h2>
-    	<nav>
-	    	<a href='#' className="nav-item">About</a>
-  	  	<a href='#' className="nav-item">Portfolio</a>
-  	  </nav>
-    </div>
-  );
+	return (
+		<div className="portfolio-nav">
+			<h2 className="name">Krisli Dimo</h2>
+			<nav>
+				<NavLink className="nav-item" to="/portfolio">Portfolio</NavLink>
+				<NavLink className="nav-item" to="/about">About</NavLink>
+			</nav>
+		</div>
+	);
 }
 
 export default PortfolioNav;
