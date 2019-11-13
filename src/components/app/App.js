a6fc53b2be483143351c11b8c5f2e7eb95f68ac8
krislidimo <krislidimo@gmail.com>
2019-11-13 16:14:16 -0600

    installed smooth-scroll

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import SmoothScroll from 'smooth-scroll';
 
 import Banner from '../banner/Banner.js'
 import Portfolio from '../portfolio/Portfolio.js'
 
-function App() {
-  return (
-    <div className="App">
-		<Banner />
-    	<Portfolio />
-    </div>
-  );
+class App extends React.Component {
+	
+	componentDidMount() {
+		var scroll = new SmoothScroll('a[href*="#"]', {
+		    speed: 2000
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Banner />
+				<Portfolio />
+			</div>
+		);
+	}
 }
 
 export default App;
