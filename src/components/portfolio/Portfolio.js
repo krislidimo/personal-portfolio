5a0ec56144efc54abaf88faac913fe6fe8832df7
krislidimo <krislidimo@gmail.com>
2019-11-13 16:19:40 -0600

    added arrow, smooth scrolls to portfolio

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { Route, Redirect} from 'react-router-dom';
 
 import PortfolioNav from './portfolioNav/PortfolioNav.js';
 import PortfolioCarousel from './portfolioCarousel/PortfolioCarousel.js';
 import About from './about/About.js';
 
-import { Route } from 'react-router-dom';
+import downarrow from '../../assets/images/down-arrow.png';
+
 
 function Portfolio() {
   return (
-    <div className="portfolio" id="portfolio">
-		<PortfolioNav />
-    	<Route exact path="/" component={PortfolioCarousel} />
-    	<Route path="/portfolio" component={PortfolioCarousel} />
-    	<Route path="/about" component={About} />
-    </div>
+  	<div className="portfolio-wrapper">
+  		<div className="arrow">
+			<a href="#portfolio">
+				<img className="arrow-img" src={downarrow} alt='down-arrow' />
+			</a>
+  		</div>
+
+	    <div className="portfolio" id="portfolio">
+			<PortfolioNav />
+			<Redirect exact from="/" to="/portfolio" />
+	    	<Route exact path="/portfolio" component={PortfolioCarousel} />
+	    	<Route exact path="/about" component={About} />
+	    </div>
+	</div>
   );
 }
 
