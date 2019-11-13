f3ace541a68929a8d6b31d751467c3d3388a9651
krislidimo <krislidimo@gmail.com>
2019-11-13 16:21:16 -0600

    made about page responsive

--- src/components/portfolio/about/About.js
+++ src/components/portfolio/about/About.js
@@ -9,10 +9,10 @@ function About() {
 			<h2 className="hello"><span>Hi, I’m Krisli.</span><span className="line2"> Nice to meet you.</span></h2>
 			<p className="about-paragraph">After I completed my bachelors in Mechanical Engineering in 2018, I got involved with a crypto-currency community called EOS. 
 				Working and debating with developers on a daily basis inspired me to become one.
-				Because of this I joined Lambda, a 9 month coding academy where I learned how to build powerful applications using agile methodologies in a team setting.
+				Because of this I joined Lambda, a 9 month (full time, 40+ hours a week) coding academy where I learned how to build powerful applications using agile methodologies in a team setting.
 				I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
-				#Let'sWork
 			</p>
+			<p className="about-paragraph">#Let'sWork</p>
 		</div>
 	);
 }
