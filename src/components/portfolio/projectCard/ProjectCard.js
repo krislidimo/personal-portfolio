b3e67d71a5e326455bc6beffc3fba3de4e71cafd
krislidimo <krislidimo@gmail.com>
2019-11-21 19:43:28 -0600

    added project links and buttons, styled buttons and adjusted styling

--- src/components/portfolio/projectCard/ProjectCard.js
+++ src/components/portfolio/projectCard/ProjectCard.js
@@ -13,12 +13,26 @@ class ProjectCard extends React.Component {
     		className="tilty"
     		settings={{
     			glare: true,
+          perspective: 3000,
+          max: 18,
     			scale: 1.04,
     			"max-glare": 0.5
     		}}
     	>
       	<div className="project-card" style={{backgroundImage: `url("${this.props.project.src}")`}}>
-          <h3 className="project-number">{this.props.project.id}</h3>
+          <h3 className="info">{this.props.project.info}</h3>
+          <h4 className="project-number">{this.props.project.id}</h4>
+          {!this.props.project.info && <button 
+            className="visit-btn"
+            onClick={() => window.open(this.props.project.siteLink, "_blank")}
+          >Visit</button>}
+          <i 
+            class="fab fa-github-square"
+            onClick={() => window.open(this.props.project.githubLink, "_blank")}
+          ></i>
+          <div className="shadow-container">
+            <div className="shadow-effect"></div>
+          </div>
      	 	</div>
      	</Tilty>
     );
