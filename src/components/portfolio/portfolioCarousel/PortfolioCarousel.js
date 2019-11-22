e212d10f806f59ff45e4086a27730618fe83b336
krislidimo <krislidimo@gmail.com>
2019-11-21 19:40:24 -0600

    added project links, commented and reorganized code

--- src/components/portfolio/portfolioCarousel/PortfolioCarousel.js
+++ src/components/portfolio/portfolioCarousel/PortfolioCarousel.js
@@ -18,7 +18,7 @@ import springlogo from "../../../assets/images/icons/spring3.png";
 const htmlIcon = <i className="fab fa-html5 fa-5x"></i>;
 const cssIcon = <i className="fab fa-css3 fa-5x"></i>;
 const jsIcon = <i className="fab fa-js-square fa-5x"></i>;
-const reactIcon = <i className="fab fa-react fa-5x"></i>
+const reactIcon = <i className="fab fa-react fa-5x"></i>;
 const nodeIcon = <i className="fab fa-node fa-5x"></i>;
 const javaIcon = <i className="fab fa-java fa-5x"></i>;
 const pythonIcon = <i className="fab fa-python fa-5x"></i>;
@@ -34,66 +34,104 @@ class PortfolioCarousel extends React.Component {
       width: window.innerWidth,
       currentSlide: {
         'title': "Nifty Market",
+        'description': 'Nifty Market was built in a 4 day hackathon style event with myself and one other developer. My role was to develop a front-end application that acted as a marketplace for virtual items. Despite there only being two of us, my partner and I managed to built a site complete with user accounts and a fully functioning market place.',
         'src': img1,
+        'siteLink': "",
+        'githubLink': "",
         'id': '01',
-        'stack': [htmlIcon,cssIcon,jsIcon,reactIcon,reduxIcon]
+        'stack': [htmlIcon,cssIcon,jsIcon,reactIcon,reduxIcon],
+        'info': ''
       },
       projects: [
         {
           'title': "It's Over 9000",
+          'description': 'My first attempt at web development was built in 4 days utilizing HTML, CSS, and JS. It was also my first time working with GSAP, a JS animation library. This site served as the marketing page for a chess player ranking application. The project was in collaboration with 8 other team members, including 3 data scientists, 2 back-end devs, and 1 front-end dev.',
           'src': img0,
+          'siteLink': "https://it-is-over9000.github.io/ItsOver9000-UI/",
+          'githubLink': "https://github.com/it-is-over9000/ItsOver9000-UI",
           'id': '00',
-          'stack': [htmlIcon,cssIcon,jsIcon,gsapIcon]
+          'stack': [htmlIcon,cssIcon,jsIcon,gsapIcon],
+          'info': ''
         },
         {
           'title': "Nifty Market",
+          'description': 'Nifty Market was built in a 4 day hackathon style event with myself and one other developer. My role was to develop a front-end application that acted as a marketplace for virtual items. Despite there only being two of us, my partner and I managed to built a site complete with user accounts and a fully functioning market place.',
           'src': img1,
+          'siteLink': "",
+          'githubLink': "https://github.com/nifty-market/Nifty-Market-FE",
           'id': '01',
-          'stack': [htmlIcon,cssIcon,jsIcon,reactIcon,reduxIcon]
+          'stack': [htmlIcon,cssIcon,jsIcon,reactIcon,reduxIcon],
+          'info': ''
         },
         {
           'title': "Mympy",
+          'description': 'Mympy was an 8 week long co-op project in collaboration with SIXR, a VR educational non-profit. This project was designed as an Indigogo style donations platform specifically for under privileged dreamers who want to get involved with vr creation. My contribution included front-end development as well as integrating Stripe, Cloudinary, and Formidable Victory.',
           'src': img2,
+          'siteLink': "https://mympydreamers.netlify.com/",
+          'githubLink': "https://github.com/mympy-dreamers/Frontend",
           'id': '02',
-          'stack': [htmlIcon,cssIcon,jsIcon,reactIcon,nodeIcon]
+          'stack': [htmlIcon,cssIcon,jsIcon,reactIcon,nodeIcon],
+          'info': ''
         },
         {
           'title': "Macro Calculator",
+          'description': 'Macro Calculator was an IOS application that calculated macro nutrients for fitness enthusiasts. My main role was to build out the back-end that housed user information. This project was built using Spring and utilized Oauth for authentication.',
           'src': "",
+          'siteLink': "",
+          'githubLink': "https://github.com/build-week-macro-calculator",
           'id': '03',
-          'stack': [javaIcon,springIcon,postgresIcon]
+          'stack': [javaIcon,springIcon,postgresIcon],
+          'info': 'Back-End Project'
         },
         {
           'title': "Treasure Hunter",
+          'description': 'For this project our team of 3 was given a set of computer science problems to solve. The three main problems where to traverse a given graph using depth first traversal, develop and use an 8bit cpu emulator to decode an encrypted message, and develop and use a proof of work mining algorithm to mine a virtual coin. My team and I completed all three task as well as created an interface for the project using React.',
           'src': img4,
+          'siteLink': "",
+          'githubLink': "https://github.com/CS21-Team12/treasure-hunt",
           'id': '04',
-          'stack': [htmlIcon,cssIcon,jsIcon,reactIcon,pythonIcon]
+          'stack': [htmlIcon,cssIcon,jsIcon,reactIcon,pythonIcon],
+          'info': ''
         }
       ]
     }
   }
 
   componentDidMount() {
+    this.initListeners()
+    this.updateCurrentProject()
+    // on window resize, calls resize function
+    window.onresize = this.resize;
+  }
+
+  // initializes listeners for when the user changes project
+  // handles clicking arrows, dots, and dragging slides
+  initListeners = () => {
     var arrows = document.querySelectorAll('.slick-arrow')
     var dots = document.querySelector('.slick-dots')
     var slides = document.querySelectorAll('.slick-slide')
+
+    // clicking carousel arrows
     arrows.forEach((btn) => {
       btn.addEventListener('click', this.updateCurrentProject)
     })
+
+    // clicking carousel dots
     for (var i = 0; i < dots.children.length; i++) {
       dots.children[i].addEventListener('click', this.updateCurrentProject)
     }
+
+    // dragging slides
     slides.forEach((slide) => {
       slide.addEventListener('mouseup', this.updateCurrentProject)
     })
-    this.updateCurrentProject()
-    window.onresize = this.resize;
   }
 
   updateCurrentProject = () => {
     setTimeout(() => {
       var activeSlides = document.querySelectorAll('.slick-active')
-      var id = parseInt(activeSlides[parseInt(activeSlides.length/2)-1].innerText.slice(-2))
+      var centerSlide = activeSlides[parseInt(activeSlides.length/2)-1]
+      var id = parseInt(centerSlide.querySelector('.project-number').innerText)
       this.setState({
         ...this.state,
         currentSlide: this.state.projects[id]
@@ -101,6 +139,7 @@ class PortfolioCarousel extends React.Component {
     },300)
   }
 
+  // updated width on resize.
   resize = () => {
     this.setState({
       ...this.state,
@@ -110,11 +149,13 @@ class PortfolioCarousel extends React.Component {
 
   
   render () {
-    console.log(this.state.width)
     var slides = 3 
+
+    // if screen width is less than 500 adjust number of slides
     if (this.state.width <= 500) {
       slides = 1
     }
+
     const settings = {
       infinite: true,
       centerPadding: "60px",
@@ -125,7 +166,7 @@ class PortfolioCarousel extends React.Component {
 
     return (
       <div className="portfolio-carousel">
-        <h2 className="title">{this.state.currentSlide.id + " - " +this.state.currentSlide.title}</h2>
+        <h2 className="title">{this.state.currentSlide.title}</h2>
 
         <Slider {...settings}>
           {this.state.projects.map(project => {
@@ -136,7 +177,7 @@ class PortfolioCarousel extends React.Component {
         </Slider>
 
         <p className="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          {this.state.currentSlide.description}
         </p>
 
         <div className="tech-stack">
